63f5f5a3a346aeedb6003ca6efd2abecb5639299
Parth Sikka <parthsikkah704@gmail.com>
2021-06-05 12:37:44 +0530

    Final Version

--- sheets.js
+++ sheets.js
@@ -1,92 +1,78 @@
-function solveFormula(formula, selfCellObject) {
-  // "( A1 + A2 )" => "( 10 + 20 )"
-  let formulaComps = formula.split(" ");
-  // ["(" , "A1" , "+" , "A2" , ")"];
+let addSheetBtn = document.querySelector(".add-sheet");
+let sheetsList = document.querySelector(".sheets-list");
+let sheetId = 0;
 
-  //this loop replace value of A1 and A2 in formula with their respective cell values !!
-  for (let i = 0; i < formulaComps.length; i++) {
-    let formComp = formulaComps[i];
-    if (formComp[0] >= "A" && formComp[0] <= "Z") {
-      // valid formula component
-      // A1 => Z100
-      let { rowId, colId } = getRowIdColIdFromAddress(formComp);
-      let cellObject = db[rowId][colId];
-      let value = cellObject.value;
-      
-      if (selfCellObject) {
-        // push yourself in the childrens of formula Components cellObject
-        cellObject.childrens.push(selfCellObject.name);
-        selfCellObject.parents.push(cellObject.name);
-      }
+addSheetBtn.addEventListener("click", function (e) {
+  sheetId++;
+  // it will remove activesheet class from current active sheet
+  document.querySelector(".active-sheet").classList.remove("active-sheet");
 
-      formula = formula.replace(formComp, value);
-    }
+  let sheetDiv = document.createElement("div");
+  sheetDiv.classList.add("sheet");
+  sheetDiv.classList.add("active-sheet");
+  sheetDiv.setAttribute("sheetid", sheetId);
+  sheetDiv.innerHTML = `Sheet ${sheetId + 1}`;
+
+  // <div class="sheet" sheetid="1">Sheet 2</div>
+  sheetsList.append(sheetDiv);
+
+  //UI should be new
+  initUI();
+
+  // new sheet db 
+  // sheetsdb.push(new sheet db)
+  // db = new sheet db
+  initDB();
+});
+
+sheetsList.addEventListener("click", function (e) {
+  let selectedSheet = e.target;
+  // active-sheet
+  if (selectedSheet.classList.contains("active-sheet")) {
+    return;
   }
+  // non active-sheet
+  // it will remove activesheet class from current active sheet
+  document.querySelector(".active-sheet").classList.remove("active-sheet");
+  selectedSheet.classList.add("active-sheet");
 
-  // Stack Infix Evaluation !!! => "( 10 + 20 )";
-  let computedValue = eval(formula);
-  return computedValue;
-}
+  initUI();
 
-function updateChildrens(cellObject) {
-  // {
-  //     name:"A1",
-  //     value:"100",
-  //     formula:"",
-  //     childrens:["B1",  "C1"]
-  // }
-  for (let i = 0; i < cellObject.childrens.length; i++) {
-    let childrenName = cellObject.childrens[i];
-    let { rowId, colId } = getRowIdColIdFromAddress(childrenName);
-    let childrenCellObject = db[rowId][colId];
-    // {
-    //     name:"B1",
-    //     value:"30",
-    //     formula:"( A1 + A2 )",
-    //     childrens:[]
+  //set current db to active sheet db;
+  let sheetId = selectedSheet.getAttribute("sheetid");
+  
+  // set db and visited cells
+  db = sheetsDB[sheetId].db;
+  visitedCells = sheetsDB[sheetId].visitedCells;
+
+  // set UI according to that db
+  setUI();
+});
+
+function setUI(){
+    // for(let i=0 ; i<100 ; i++){
+    //     for(let j=0 ; j<26 ; j++){
+    //         let cellObject = db[i][j];
+    //         document.querySelector(`div[rowid="${i}"][colid="${j}"]`).textContent = cellObject.value; 
+    //     }
     // }
-    let newValue = solveFormula(childrenCellObject.formula);
-    
-    // ui update
-    document.querySelector(`div[rowid='${rowId}'][colid='${colId}']`).textContent = newValue;
-    // db update
-    childrenCellObject.value = newValue;
-    updateChildrens(childrenCellObject);
-  }
+    for(let i=0 ; i<visitedCells.length ; i++){
+      let {rowId , colId} = visitedCells[i];
+      let cellObject = db[rowId][colId];
+      document.querySelector(`div[rowid="${rowId}"][colid="${colId}"]`).innerHTML = cellObject.value;
+    }
 }
 
-function removeFormula(cellObject){
-  cellObject.formula = "";
-  for(let i=0 ; i<cellObject.parents.length ; i++){
-    let parentName = cellObject.parents[i];
-    let {rowId , colId} = getRowIdColIdFromAddress(parentName);
-    let parentCellObject = db[rowId][colId];
-
-    let updatedChildrens = parentCellObject.childrens.filter(function(children){
-      return children != cellObject.name;
-    })
-
-    parentCellObject.childrens = updatedChildrens;
-  }
-  cellObject.parents = [];
-}
-
-function getRowIdColIdFromElement(element) {
-  let rowId = element.getAttribute("rowid");
-  let colId = element.getAttribute("colid");
-  return {
-    rowId,
-    colId,
-  };
-}
-
-function getRowIdColIdFromAddress(address) {
-  // B2 => colid,rowId
-  // B => 1
-  let rowId = Number(address.substring(1)) - 1;
-  let colId = address.charCodeAt(0) - 65;
-  return {
-    rowId,
-    colId,
-  };
+function initUI(){
+    // for(let i=0 ; i<100 ; i++){
+    //     for(let j=0 ; j<26 ; j++){
+    //         document.querySelector(`div[rowid="${i}"][colid="${j}"]`).innerHTML = "";
+    //     }
+    // }
+    for(let i=0 ; i<visitedCells.length ; i++){
+      let {rowId , colId} = visitedCells[i];
+      let cell = document.querySelector(`div[rowid="${rowId}"][colid="${colId}"]`);
+      cell.innerHTML = "";
+      cell.style = "";
+    }
 }
--- util.js
+++ util.js
@@ -1,63 +1,92 @@
-function calculate(formula, selectedCellObject){
-    let comps = formula.split(" ") ;
-    for(let a=0; a<comps.length ; a++){
-        //"[", "A1", "+", "B1", "]"
-        let c = comps[a]; 
-        if(c[0]>="A" && c[0]<="Z"){
-            let {rowid, colid} = convertToCoordinates(c) ;
-           // console.log(rowid, colid);
-            let cellObject = db[rowid][colid] ; 
-            let value = cellObject.value ;
-            if(selectedCellObject){
-            cellObject.childrens.push(selectedCellObject.name) ;
-            selectedCellObject.parents.push(c) ;
-            }
-            formula = formula.replace(c, value) ;
-        }
+function solveFormula(formula, selfCellObject) {
+  // "( A1 + A2 )" => "( 10 + 20 )"
+  let formulaComps = formula.split(" ");
+  // ["(" , "A1" , "+" , "A2" , ")"];
+
+  //this loop replace value of A1 and A2 in formula with their respective cell values !!
+  for (let i = 0; i < formulaComps.length; i++) {
+    let formComp = formulaComps[i];
+    if (formComp[0] >= "A" && formComp[0] <= "Z") {
+      // valid formula component
+      // A1 => Z100
+      let { rowId, colId } = getRowIdColIdFromAddress(formComp);
+      let cellObject = db[rowId][colId];
+      let value = cellObject.value;
+      
+      if (selfCellObject) {
+        // push yourself in the childrens of formula Components cellObject
+        cellObject.childrens.push(selfCellObject.name);
+        selfCellObject.parents.push(cellObject.name);
+      }
+
+      formula = formula.replace(formComp, value);
     }
-    
-    let ans = eval(formula) ;
-    return ans ; 
+  }
+
+  // Stack Infix Evaluation !!! => "( 10 + 20 )";
+  let computedValue = eval(formula);
+  return computedValue;
 }
 
-function convertToCoordinates(ch){
-    // B1 -> {1,1}
-    colid = ch.charCodeAt(0) - 65 ;
-    rowid = Math.abs(Number(ch.substring(1))-1) ;
-    return {rowid,colid} ;
-}
-
-function updateChildrens(cellObject){
+function updateChildrens(cellObject) {
+  // {
+  //     name:"A1",
+  //     value:"100",
+  //     formula:"",
+  //     childrens:["B1",  "C1"]
+  // }
+  for (let i = 0; i < cellObject.childrens.length; i++) {
+    let childrenName = cellObject.childrens[i];
+    let { rowId, colId } = getRowIdColIdFromAddress(childrenName);
+    let childrenCellObject = db[rowId][colId];
+    // {
+    //     name:"B1",
+    //     value:"30",
+    //     formula:"( A1 + A2 )",
+    //     childrens:[]
+    // }
+    let newValue = solveFormula(childrenCellObject.formula);
     
-    for(let a=0 ; a<cellObject.childrens.length ; a++){
-        let currentCell = cellObject.childrens[a] ;
-       
-        let {rowid,colid} = convertToCoordinates(currentCell) ;
-        
-        let currentCellObject = db[rowid][colid] ;
-        let Formula = currentCellObject.formula ;
-        let ans = calculate(Formula) ;
-        // Update DB : 
-        currentCellObject.value = ans ;
-        // Update UI : 
-        document.querySelector(`.cell[rowid="${rowid}"][colid="${colid}"]`).textContent = ans ;
-        
-        updateChildrens(currentCellObject) ;
-} 
+    // ui update
+    document.querySelector(`div[rowid='${rowId}'][colid='${colId}']`).textContent = newValue;
+    // db update
+    childrenCellObject.value = newValue;
+    updateChildrens(childrenCellObject);
+  }
 }
 
 function removeFormula(cellObject){
-    cellObject.formula="" ;
-    for(let b=0 ; b<cellObject.parents.length ; b++){
-        // A1, A2
-        let {rowid, colid} = convertToCoordinates(cellObject.parents[b]) ;
-        let fatherCell = db[rowid][colid] ;
-        for(let i=0 ; i<fatherCell.childrens.length ; i++){
-            if(fatherCell.childrens[i]==(cellObject.name)){
-                fatherCell.childrens.splice(i,1) ;
-            }
-        }
-    }
+  cellObject.formula = "";
+  for(let i=0 ; i<cellObject.parents.length ; i++){
+    let parentName = cellObject.parents[i];
+    let {rowId , colId} = getRowIdColIdFromAddress(parentName);
+    let parentCellObject = db[rowId][colId];
 
-    cellObject.parents = [] ;
+    let updatedChildrens = parentCellObject.childrens.filter(function(children){
+      return children != cellObject.name;
+    })
+
+    parentCellObject.childrens = updatedChildrens;
+  }
+  cellObject.parents = [];
+}
+
+function getRowIdColIdFromElement(element) {
+  let rowId = element.getAttribute("rowid");
+  let colId = element.getAttribute("colid");
+  return {
+    rowId,
+    colId,
+  };
+}
+
+function getRowIdColIdFromAddress(address) {
+  // B2 => colid,rowId
+  // B => 1
+  let rowId = Number(address.substring(1)) - 1;
+  let colId = address.charCodeAt(0) - 65;
+  return {
+    rowId,
+    colId,
+  };
 }
